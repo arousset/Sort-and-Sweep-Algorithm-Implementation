96730f63c87f6525ea8c884676fe5b34fb980504
bbarlowAIEYr1 <bbarlow731@gmail.com>
2015-12-28 20:00:55 -0600

    rect class creation
    added a rect class to the sort and sweep file

--- Python/sweep&prune.py
+++ Python/sweep&prune.py
@@ -1,3 +1,12 @@
 import vectors
 from vectors import *
 
+class rect(object):
+	def __init__(self, nX, nY, nH, nW, nN):
+		self.x = nX
+		self.y = nY
+		self.h = nH
+		self.w = nW
+		self.n = nN
+		self.min = {self.x - (self.w / 2), self.y - (self.h / 2)}
+		self.max = {self.x + (self.w / 2), self.y + (self.h / 2)}
