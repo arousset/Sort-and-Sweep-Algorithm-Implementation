c164109a3e345a633656f48edc8d74817e55063c
bbarlowAIEYr1 <bbarlow731@gmail.com>
2015-12-28 20:21:13 -0600

    vector class creation
    created a templated vector class for 2D, 3D, and 4D vectors

---
+++ C++/SweepPrune/SweepPrune/vectors.h
@@ -0,0 +1,167 @@
+#pragma once
+#include <iostream>
+#include <cstring>
+#include <cmath>
+using namespace std;
+
+template<typename T>
+class Vector 
+{
+public:
+	T x = 0;
+	T y = 0;
+	T z = 0;
+	T alpha = 0;
+
+	void Print()
+	{
+		cout << x << " " << y << " " << z << endl;
+	}
+
+	Vector()
+	{
+		x = 0;
+		y = 0;
+		z = 0;
+		alpha = 0;
+	}
+
+	Vector(T a, T b)
+	{
+		x = a;
+		y = b;
+	}
+
+	Vector(T a, T b, T c)
+	{
+		x = a;
+		y = b;
+		z = c;
+	}
+
+	Vector(T a, T b, T c, T d)
+	{
+		x = a;
+		y = b;
+		z = c;
+		alpha = d;
+	}
+
+	Vector operator+(Vector other)
+	{
+		Vector temp(0, 0);
+		temp.x = x + other.x;
+		temp.y = y + other.y;
+		temp.z = z + other.z;
+		return temp;
+	}
+
+	Vector operator-(Vector other)
+	{
+		Vector temp(0, 0);
+		temp.x = x - other.x;
+		temp.y = y - other.y;
+		temp.z = z - other.z;
+		return temp;
+	}
+
+	double Magnitude()
+	{
+		int tempx = x * x;
+		int tempy = y * y;
+		int tempz = z * z;
+		double sr = sqrt(tempx + tempy + tempz);
+
+		return sr;
+	}
+
+	Vector Nomalize()
+	{
+		double div;
+		if (x >= y && x >= z)
+		{
+			div = x;
+		}
+
+		else if (y >= x && y >= z)
+		{
+			div = y;
+		}
+
+		else
+		{
+			div = z;
+		}
+		double newx = x / div;
+		double newy = y / div;
+		double newz = z / div;
+
+		Vector temp(newx, newy, newz);
+		return temp;
+	}
+
+	double operator*(Vector other) 
+	{
+		Vector temp(0, 0);
+		temp.x = x * other.x;
+		temp.y = y * other.y;
+		temp.z = z * other.z;
+		double dot = temp.x + temp.y + temp.z;
+
+		return dot;
+	}
+
+	Vector operator/(Vector other)
+	{
+		double crossx, crossy, crossz;
+		crossx = (y * other.z) - (other.y * z);
+		crossy = (z * other.x) - (other.z * x);
+		crossz = (x * other.y) - (other.x * y);
+
+		Vector cross(crossx, crossy, crossz);
+		return cross;
+	}
+
+	double Angle(Vector b)
+	{
+		double base, opp, hyp;
+		opp = this->Magnitude();
+		base = b.Magnitude();
+		hyp = sqrt((base * base) + (opp * opp));
+
+		Vector tri(base, opp, hyp);
+		tri = tri.Nomalize();
+
+		double angle = acos(base / hyp);
+
+		angle = (angle / 3.14) * 180;
+
+		return angle;
+	}
+
+	double LinearInterpolation(float a, float b, float perc)
+	{
+		return (a + (b - a)) * perc;
+	}
+
+	float DtoR(int d)
+	{
+		return (d / 180)*3.14;
+	}
+
+	float RtoD(int r)
+	{
+		return (r / 3.14) * 180;
+	}
+
+	Vector<int> HexColor(unsigned int HexColor)
+	{
+		unsigned int Hexred = HexColor >> 24;
+		unsigned int Hexgreen = (HexColor >> 16) & 0x00FF;
+		unsigned int Hexblue = (HexColor >> 8) & 0x0000FF;
+		unsigned int Hexalpha = HexColor & 0x000000FF;
+		Vector<int> hex(Hexred, Hexgreen, Hexblue, Hexalpha);
+
+		return hex;
+	}
+};
