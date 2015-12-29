9ff6b323f62ee7d36a433ff202711e56870b8069
bbarlowAIEYr1 <bbarlow731@gmail.com>
2015-12-28 19:26:12 -0600

    test addition
    added addition to test file.

--- Python/testVectors.py
+++ Python/testVectors.py
@@ -1,3 +1,16 @@
 import vectors
 from vectors import *
 
+print "Vector 2D maths:"
+a = Vec2D(6,6)
+b = Vec2D(12,12)
+
+print "Vectors A and B:"
+a.info()
+b.info()
+print " "
+c = a + b
+print "addition:"
+c.info()
+print " "
+
